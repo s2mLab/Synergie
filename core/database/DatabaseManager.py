62c172b366e9e67163e6cd3debc36bd3309e175a
Tristan12x <tristandardiat1208@gmail.com>
2024-06-07 10:06:34 -0400

    Ajout d'une BDD Firebase

--- core/database/DatabaseManager.py
+++ core/database/DatabaseManager.py
@@ -1,5 +1,7 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 import numpy as np
-from cs50 import SQL
 
 from dataclasses import dataclass
 from datetime import datetime
@@ -26,46 +28,114 @@ class SkaterData:
 
 class DatabaseManager:
     def __init__(self):
-        self.db = SQL("sqlite:///database/database.db")
-    
+        cred = credentials.Certificate('s2m-skating-firebase-adminsdk-3ofmb-8552d58146.json')
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://s2m-skating-default-rtdb.firebaseio.com/'
+        }
+        )
+
+        self.ref = db.reference('/')
+
     def save_skater_data(self, data : SkaterData) -> int:
-        new_id = self.db.execute("INSERT INTO skater (skater_name) VALUES (?)", data.skater_name)
+        if self.ref.child("Skater").get() is None:
+            newKey = self.ref.child("Skater").push().key
+            self.ref.child("Skater").update({newKey:
+                {"skater_id" : 1,
+                "skater_name" : data.skater_name}
+            })
+        else:
+            newId = self.ref.child("Skater").order_by_key().get().popitem()[1].get("skater_id")+1
+            newKey = self.ref.child("Skater").push().key
+            self.ref.child("Skater").update({newKey : 
+                {"skater_id" : newId,
+                "skater_name" : data.skater_name}
+            }) 
+        new_id = self.ref.child("Skater").order_by_key().get().popitem()[1].get("skater_id")
         return new_id
     
     def save_training_data(self, data : TrainingData) -> int:
-        new_id = self.db.execute("INSERT INTO training (skater_id, training_date) VALUES (?,?)", data.skater_id, data.training_date)
+        if self.ref.child("Training").get() is None:
+            newKey = self.ref.child("Training").push().key
+            self.ref.child("Training").update({newKey :
+                {"training_id" : 1,
+                "skater_id" : data.skater_id,
+                "training_date" : data.training_date.isoformat(sep=' ', timespec='minutes')}
+            })
+        else:
+            newId = self.ref.child("Training").order_by_key().get().popitem()[1].get("training_id")+1
+            newKey = self.ref.child("Training").push().key
+            self.ref.child("Training").update({newKey :
+                {"training_id" : newId,
+                "skater_id" : data.skater_id,
+                "training_date" : data.training_date.isoformat(sep=' ', timespec='minutes')}
+            }) 
+        new_id = self.ref.child("Training").order_by_key().get().popitem()[1].get("training_id")
         return new_id
     
     def save_jump_data(self, data : JumpData) -> int:
-        new_id = self.db.execute("INSERT INTO jump (training_id, jump_type, jump_rotations, jump_success, jump_time) VALUES (?,?,?,?,?)", data.training_id, data.jump_type, data.jump_rotations, data.jump_success, data.jump_time)
+        if self.ref.child("Jump").get() is None:
+            newKey = self.ref.child("Jump").push().key
+            self.ref.child("Jump").update({newKey :
+                {"jump_id" : 1,
+                "training_id" : data.training_id,
+                "jump_type" : data.jump_type,
+                "jump_rotations" : data.jump_rotations,
+                "jump_success" : data.jump_success,
+                "jump_time" : data.jump_time}
+            })
+        else:
+            newId = self.ref.child("Jump").order_by_key().get().popitem()[1].get("jump_id")+1
+            newKey = self.ref.child("Jump").push().key
+            self.ref.child("Jump").update({newKey :
+                {"jump_id" : newId,
+                "training_id" : data.training_id,
+                "jump_type" : data.jump_type,
+                "jump_rotations" : data.jump_rotations,
+                "jump_success" : data.jump_success,
+                "jump_time" : data.jump_time}
+            }) 
+        new_id = self.ref.child("Jump").order_by_key().get().popitem()[1].get("jump_id")
         return new_id
 
     def load_skater_data(self, skater_id : int) -> list[TrainingData]:
-        data_trainings = self.db.execute("SELECT training_id, skater_id, training_date FROM training WHERE skater_id == ? ORDER BY training_date", skater_id)
-        if len(data_trainings)>0:
-            data_trainings = np.vectorize(lambda x : TrainingData(int(x["training_id"]), int(x["skater_id"]), datetime.strptime(x["training_date"], '%Y-%m-%d %H:%M:%S')))(data_trainings)
+        data_trainings = []
+        for val in self.ref.child("Training").order_by_key().get().values():
+            if val["skater_id"] == skater_id:
+                data_trainings.append(TrainingData(int(val["training_id"]), int(val["skater_id"]), datetime.strptime(val["training_date"], '%Y-%m-%d %H:%M')))
+        data_trainings = np.array(data_trainings)
         return data_trainings
 
     def load_training_data(self, training_id : int) -> list[JumpData]:
-        data_jumps= self.db.execute("SELECT jump_id, training_id, jump_type, jump_rotations, jump_success, jump_time FROM jump WHERE training_id == ? ORDER BY jump_time", training_id)
-        if len(data_jumps)>0:
-            data_jumps = np.vectorize(lambda x : JumpData(int(x["jump_id"]), int(x["training_id"]), int(x["jump_type"]), float(x["jump_rotations"]), bool(x["jump_success"]), int(x["jump_time"])))(data_jumps)
+        data_jumps = []
+        for val in self.ref.child("Jump").order_by_key().get().values():
+            if val["training_id"] == training_id:
+                data_jumps.append(JumpData(int(val["jump_id"]), int(val["training_id"]), int(val["jump_type"]), float(val["jump_rotations"]), bool(val["jump_success"]), int(val["jump_time"])))
+        data_jumps = np.array(data_jumps)
         return data_jumps
     
     def get_skater_from_training(self, training_id : int) -> int:
-        name = self.db.execute("SELECT skater_id FROM skater NATURAL JOIN training WHERE training_id == ?", training_id)
+        name = []
+        for val in self.ref.child("Training").order_by_key().get().values():
+            if val["training_id"] == training_id:
+                name.append(val)
         return int(name[0]["skater_id"])
 
     def get_skater_name_from_id(self, skater_id : int) -> str:
-        name = self.db.execute("SELECT skater_name FROM skater WHERE skater_id == ?", skater_id)
+        name = []
+        for val in self.ref.child("Skater").order_by_key().get().values():
+            if val["skater_id"] == skater_id:
+                name.append(val)
         return name[0]["skater_name"]
     
     def get_all_skaters(self) -> list[SkaterData]:
-        data_skaters = self.db.execute("SELECT skater_id, skater_name FROM skater ")
-        if len(data_skaters)>0:
-            data_skaters = np.vectorize(lambda x : SkaterData(int(x["skater_id"]), x["skater_name"]))(data_skaters)
+        data_skaters = []
+        for val in self.ref.child("Skater").order_by_key().get().values():
+            data_skaters.append(SkaterData(int(val["skater_id"]), val["skater_name"]))
         return data_skaters
     
     def delete_skater_data(self, skater_id : int) -> None:
-        self.db.execute("DELETE FROM skater WHERE skater_id == ?", skater_id)
-        
+        deleted_key = 0
+        for key, val in self.ref.child("Skater").order_by_key().get().items():
+            if int(val["skater_id"]) == int(skater_id):
+                deleted_key = key
+        self.ref.child(f"Skater/{deleted_key}").delete()
